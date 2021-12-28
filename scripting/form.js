964b33b96993723c25cd872ea1a22e795f676f6b
DINESH <dineshsubramani0@gmail.com>
2021-12-28 23:23:22 +0530

    form

---
+++ scripting/form.js
@@ -0,0 +1,14 @@
+
+// function getValuefname(){
+//     var firstname=(document.getElementById("fname").value)
+//     console.log(firstname)
+// }
+
+// function getValuemname(){
+//     console.log(document.getElementById("mname").value)
+// }
+
+// function getValuelname(){
+//     console.log(document.getElementById("lname").value)
+// }
+
