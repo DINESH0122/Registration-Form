c00655ea23355127a810c9d018bb836c88c9d5ec
DINESH <dineshsubramani0@gmail.com>
2021-12-29 13:40:35 +0530

    first commit

--- scripting/form.js
+++ scripting/form.js
@@ -1,8 +1,8 @@
 
-// function getValuefname(){
-//     var firstname=(document.getElementById("fname").value)
-//     console.log(firstname)
-// }
+function getValuefname(){
+    var firstname= document.getElementById("fname").value;
+    console.log(firstname)
+}
 
 // function getValuemname(){
 //     console.log(document.getElementById("mname").value)
